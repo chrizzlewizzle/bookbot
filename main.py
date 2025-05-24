7b8f96b25b7f831bf5e8616a5cb951e84eacc060
chrizzlewizzle <cvdwal@gmail.com>
2025-05-24 19:46:01 +0200

    CH3 L1 Print a report

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_chars = get_num_chars(text)
-    list_chars = dict_to_list(num_chars)
-    ordered_list = order_list(list_chars)
-    
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-    print("--- Begin report of ", book_path," ---")
-    print(f"{num_words} words found in the document")
-    ordered_list
-    print("--- End report ---")
-
-def order_list(list):
-    list.sort(key=lambda x: x["num"], reverse=True) # sorts the list based on the num (2nd thing in the list)
-    
-    for char in list:
-        letter = char["letter"]
-        count = char["num"]
-        print(f"The '{letter}' character was found {count} times")
-        
-
-def dict_to_list(dict):
-    list = [{"letter": char, "num": count} for char, count in dict.items()] # magic line: it takes a dictionary with key-value pairs, takes the key and names it char, value is count, and then creates a list with letter = char and num = count
-    return list
-
-def get_num_chars(text):
-    count = {} # empty dictionary
-    lowered_text = text.lower() # make input lowercase
-
-    for char in lowered_text: # iterate over each character in the text
-        if char.isalpha(): # make sure the character is in the alphabet
-                if char in count: # if the character already exists in dictionary, increase number
-                    count[char] += 1
-                else: # if character doesn't already exist in dictionary, set number to 1
-                    count[char] = 1
-    return count
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
